515e7b0793b070e9e98e25efe19916b58f989a2c
Roberta Aguilar <roberta.aguilar@kobel.com.br>
2021-06-27 12:17:44 -0300

    Home My Skills

---
+++ mySkills/src/pages/Home.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable prettier/prettier */
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  TouchableOpacity, //quando clicar nele vai ficar um pouco transparente
+} from 'react-native';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, Roberta</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder={'New Skill'}
+        placeholderTextColor={'#999'}
+      />
+
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+
+      <Text style={[styles.title, {marginTop: 50}]}>My Skills</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 30,
+    paddingVertical: 70,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#6d6d6d',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  button: {
+    backgroundColor: '#A370F7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+});
